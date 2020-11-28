c5a5867663503821373ea8b6b6e699d1639c5b66
GeorgioSim <simgeorge9@gmail.com>
2020-11-29 01:01:25 +0200

    Create P3.R

---
+++ askiseis ergastiriou kormou 1/P3.R
@@ -0,0 +1,49 @@
+#Ergastirio kormou 1 askisi P3
+#Simopoulos Georgios, AM:1110201800159
+library(data.table)
+library(purrr)
+library(readxl)
+library("writexl")
+library(ggplot2)
+library(dplyr)
+library(broom)
+library(tidyselect)
+library(ggthemes)
+library(ggrepel)
+library(ggpubr)
+args(ggplot)
+
+Rdata<-read_excel("proxeiro.xlsx")
+View(Rdata)
+
+DIR<-Rdata$DIR
+SPD<-Rdata$`SPD[m/s]`
+rm(xdata)
+#1,2,3....12
+Tasks<- "Tasks"
+Month<-c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+
+s<-SPD[which(Rdata$MO==1)]
+Apnoia<-length(which(s==0))
+Mesi_timi<-mean(s)
+Diaspora<-var(s)
+Diamesos<-median(s)
+xdata <- data.frame(Tasks = c("Apnoia","Average","Variance","Median"), 
+                    Jan = c(Apnoia,Mesi_timi,Diaspora,Diamesos),
+                    stringsAsFactors = FALSE)
+
+for(i in 2:12){
+  s<-SPD[which(Rdata$MO==i)]
+  Apnoia<-length(which(s==0))
+  Mesi_timi<-mean(s)
+  Diaspora<-var(s)
+  Diamesos<-median(s)
+  xdata <- data.frame(xdata, 
+                      dec=c(Apnoia,Mesi_timi,Diaspora,Diamesos),
+                      stringsAsFactors = FALSE)
+}
+
+setnames(xdata, old = colnames(xdata), new = c(Tasks,Month))
+View(xdata)
+
+rm(xdata)
