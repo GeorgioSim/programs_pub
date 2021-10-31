867985b7333d7bf5a574ec47022da0c073044875
GeorgioSim <simgeorge9@gmail.com>
2021-10-31 09:45:37 +0200

    update transfer

--- Projects/Ptyxiaki/Chapter_1/1.1/better/pulses.m
+++
@@ -1,6 +0,0 @@
-function [ex,spread]=pulses(ks1,ks2,t,t0,ex,spread)
-pulse=exp(-.5*((t-t0)/spread)^2);
-ex(ks1)=pulse;
-pulse2=exp(-.5*((t-t0)/spread)^2);
-ex(ks2)=pulse2;
-end
--- Projects/Ptyxiaki/Chapter_1/1.3/better/pulse.m
+++
@@ -1,5 +0,0 @@
-function [ex,spread]=pulse(ks,t,t0,ex,spread)
-% Source
-pulsar=exp(-.5*((t-t0)/spread)^2);
-ex(ks)=ex(ks)+pulsar;
-end
